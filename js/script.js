5816bd048e3aa7e28be4d3a1d617288a66f4a088
asharameta <1sharameta@gmail.com>
2024-01-29 16:27:14 +0100

    unzip and print epub book cover

--- js/script.js
+++ js/script.js
@@ -1,32 +1,62 @@
-
-
-window.onload = function() {
-    console.log( "ready!" );
+window.onload = function(e) {
+    console.log("ready!");
     eventHandler();
 };
 
 function eventHandler(){
     $(".dropZone").on("drop", (e) => {
-        // Prevent default behavior to allow drop
-        e.preventDefault();
+        if(e.cancelable)e.preventDefault();
 
         console.log("Drop event on .dropZone");
         let dataTransfer = e.originalEvent.dataTransfer;
-        if([...dataTransfer.items].forEach((item, i) => {
-            // If dropped items aren't files, reject them
-            if (item.type === "application/epub+zip") {
-              const file = item.getAsFile();
-              console.log(`file name = ${file.name}`);
-            }else{
-                console.log("WRONG FILE FORMAT WE TAKE ONLY EPUB AT THE MOMENT");
-            }
-          }));
+
+        if (dataTransfer.items[0].type === "application/epub+zip") {
+            const file = dataTransfer.items[0].getAsFile();
+            console.log(`file name = ${file.name}`);
+
+            showEpub(file);
+        }
     });
     
     $(".dropZone").on("dragover", (e) => {
-        // Prevent default behavior to allow drop
-        e.preventDefault();
-    
+        if(e.cancelable)e.preventDefault();
         console.log("Dragover event on .dropZone");
     });
 }
+
+
+function showEpub(file){
+    const zip = new JSZip();
+    
+    zip.loadAsync(file)
+            .then(zip => {
+                const coverPath = findCoverPath(zip);
+                
+                    if (coverPath) {
+                        zip.file(coverPath).async('base64').then(content =>{
+                            const coverImage = document.getElementById('coverImage');
+                            coverImage.src = 'data:image/png;base64,' + content;
+                            coverImage.style.display = 'block';
+                        });
+                    }else {
+                        alert('No cover image found in the EPUB file.');
+                    }
+            })
+            .catch(error => {
+                console.error("Error reading EPUB file:", error);
+            });
+}
+
+function findCoverPath(zip){
+    const coverExtensions = [".jpg", ".jpeg", ".png"];
+    for(const ext of coverExtensions) {
+        for(const obj in zip.files){
+            if(obj.includes('cover'+ext)){
+                return obj;
+            }
+        }
+    }
+    return null;
+}
+
+
