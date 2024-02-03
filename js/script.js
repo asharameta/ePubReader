eca1bf7e28b352a0b6eddc58eaf4fb003f2fa57e
asharameta <1sharameta@gmail.com>
2024-02-03 14:05:52 +0100

    added couple of new books, also along with cover print also first chapter of the book

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function eventHandler(){
             const file = dataTransfer.items[0].getAsFile();
             console.log(`file name = ${file.name}`);
 
-            showEpub(file);
+            unzipEpub(file);
         }
     });
     
@@ -25,21 +25,40 @@ function eventHandler(){
 }
 
 
-function showEpub(file){
+function unzipEpub(file){
     const zip = new JSZip();
     
     zip.loadAsync(file)
             .then(zip => {
                 const coverPath = findCoverPath(zip);
-                
+                const firstPagePath = findFirstPagePath(zip);
                     if (coverPath) {
                         zip.file(coverPath).async('base64').then(content =>{
                             const coverImage = document.getElementById('coverImage');
                             coverImage.src = 'data:image/png;base64,' + content;
-                            coverImage.style.display = 'block';
+                            coverImage.style.height = 400+'px';
                         });
                     }else {
                         alert('No cover image found in the EPUB file.');
+                    }
+
+                    if(firstPagePath){
+                        zip.file(firstPagePath).async("string").then(content =>{
+                            const parser = new DOMParser();
+                            //console.log(content);
+                            const xmlDoc = parser.parseFromString(content, "application/xml");
+                            //"application/xhtml+xml"
+                            //"application/xml"
+                            //"text/html"
+                            //"text/xml"
+                            const pagesToPrint = document.getElementById('bookContent');
+                            //console.log(xmlDoc.documentElement);
+                            pagesToPrint.innerHTML = '';
+                            pagesToPrint.appendChild(xmlDoc.documentElement);
+                            pagesToPrint.style.width = 500+'px';
+                        });
+                    }else {
+                        alert('No first page found in the EPUB file.');
                     }
             })
             .catch(error => {
@@ -52,6 +71,19 @@ function findCoverPath(zip){
     for(const ext of coverExtensions) {
         for(const obj in zip.files){
             if(obj.includes('cover'+ext)){
+                return obj;
+            }
+        }
+    }
+    return null;
+}
+
+function findFirstPagePath(zip){
+    console.log(zip.files);
+    const pageExtensions = [".xhtml", ".html"];
+    for(const ext of pageExtensions) {
+        for(const obj in zip.files){
+            if(obj.includes('1-1'+ext)){
                 return obj;
             }
         }
